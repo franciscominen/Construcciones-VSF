6efc431da1cda3e045ae0e06427375b21b2e6c52
Francisco Minen <fran.minen@gmail.com>
2022-06-01 00:57:12 -0300

    build test

--- dist/_nuxt/static/1653877255/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontact","\u002Fadmin","\u002F","\u002Fteam","\u002Fconstructions","\u002Fmodels"]})
--- dist/_nuxt/static/1653877255/models/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/models", {data:[{}],fetch:{},mutations:[]});
